4f7b813be82ea700bcc5ba1b1231d4138ed0d57b
Pedro Andrade <pfandrade10@hotmail.com>
2021-06-08 14:09:10 -0300

    exercício 4

---
+++ EcommerceWebAula/EcommerceWebAula/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EcommerceWebAula.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EcommerceWebAula.Areas.Identity.IdentityHostingStartup))]
+namespace EcommerceWebAula.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EcommerceWebAulaContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EcommerceWebAulaContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EcommerceWebAulaContext>();
+            });
+        }
+    }
+}
---
+++ TodoAPI/TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿#define First
+
+namespace TodoAPI.Models
+{
+#if First
+    
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+
+#else
+    // Use this to test you can over-post
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+    }
+#endif
+}
