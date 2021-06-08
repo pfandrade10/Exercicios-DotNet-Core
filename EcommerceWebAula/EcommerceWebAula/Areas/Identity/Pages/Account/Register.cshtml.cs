19ab362676c41c248bdd08254edd1d61288f49f8
Pedro Andrade <pfandrade10@hotmail.com>
2021-06-08 20:53:33 -0300

    exercicio 03 (em andamento)

--- EcommerceWebAula/EcommerceWebAula/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EcommerceWebAula/EcommerceWebAula/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -79,30 +79,49 @@ namespace EcommerceWebAula.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("Usercreateda new accountwithpassword."); 
 
-                    await _userManager.AddClaimAsync(user, new Claim("Tipo", "Admin"));
+                    await _userManager.AddClaimAsync(user, new Claim("Tipo", "User"));
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
+
+                    var callbackUrl = Url.Page("/Account/ConfirmEmail", 
                         pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        values: new { userId = user.Id, code = code }, 
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, 
+                        "Confirmyouremail", 
+                        $"Pleaseconfirmyouraccountby<a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clickinghere</a>."); 
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    await _signInManager.SignInAsync(user, isPersistent: false); 
+
+                    return LocalRedirect(returnUrl);
+
+                    //_logger.LogInformation("User created a new account with password.");
+
+                    //await _userManager.AddClaimAsync(user, new Claim("Tipo", "Admin"));
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    //}
+                    //else
+                    //{
+                    //    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //    return LocalRedirect(returnUrl);
+                    //}
                 }
                 foreach (var error in result.Errors)
                 {
